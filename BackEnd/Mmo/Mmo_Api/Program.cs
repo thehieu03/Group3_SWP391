b47672a98121f33359e7fb7015871f46f67f8d2b
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-09-19 23:11:20 +0700

    Add backend & frontend folders

---
+++ BackEnd/Mmo/Mmo_Api/Program.cs
@@ -0,0 +1,28 @@
+namespace Mmo_Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddAuthorization();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapControllers();
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.Run();
+    }
+}
