26ab6370fd8bf82f07538197b3e9df94d99a01bf
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-10-14 22:46:46 +0700

    role test

--- BackEnd/Mmo/Mmo_Api/ApiController/AccountController.cs
+++ BackEnd/Mmo/Mmo_Api/ApiController/AccountController.cs
@@ -1,18 +1,24 @@
-﻿using Mmo_Application.Services.Interface;
+﻿using Microsoft.AspNetCore.OData.Query;
+using Mmo_Application.Services.Interface;
 
 namespace Mmo_Api.ApiController;
 
-[Route("api/[controller]")]
+[Route("api/account")]
 [ApiController]
 public class AccountController : ControllerBase
 {
     private readonly IAccountServices _accountServices;
+    private readonly IMapper _mapper;
 
-    public AccountController(IAccountServices accountServices)
+    public AccountController(IAccountServices accountServices,IMapper mapper)
     {
-                                                              _accountServices = accountServices;
+        _accountServices = accountServices;
+        _mapper = mapper;
     }
     [HttpGet]
+    [EnableQuery]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetAllAccounts()
     {
         var accounts = await _accountServices.GetAllAsync();
