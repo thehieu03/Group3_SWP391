53b1708807b453d2ae9b1aa0cd5945bea9f7a2f7
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-10-16 00:08:16 +0700

    de axios

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import type { CategoriesResponse } from "../../models/modelResponse/CategoriesResponse.tsx";
 import { categoryServices } from "../../services/CategoryServices.tsx";
+import axios from "axios";
 
 const Home = () => {
   const [categories, setCategories] = useState<CategoriesResponse[]>([]);
   useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
     const fetchApi = async () => {
       try {
         const result = await categoryServices.getAllCategory();
@@ -19,10 +28,11 @@ const Home = () => {
   console.log(categories);
   return (
     <div>
-      <h1>{categories.map((item)=>{
-          return <div key={item.id}>{item.name}</div>
-      })}</h1>
-
+      <h1>
+        {categories.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </h1>
     </div>
   );
 };
