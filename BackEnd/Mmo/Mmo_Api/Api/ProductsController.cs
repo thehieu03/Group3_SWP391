1099b217e1ab2cedf2fc3fc9aaa002d321e9a3a0
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-10-18 01:09:55 +0700

    add productd

--- BackEnd/Mmo/Mmo_Api/Api/ProductsController.cs
+++ BackEnd/Mmo/Mmo_Api/Api/ProductsController.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Mmo_Application.Services.Interface;
 
-namespace Mmo_Api.Api
+namespace Mmo_Api.Api;
+
+[Route("api/products")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly IProductServices _productServices;
+    private readonly IMapper _mapper;
+
+    public ProductsController(IProductServices productServices, IMapper mapper)
     {
+        _productServices = productServices;
+        _mapper = mapper;
+    }
+    [HttpGet]
+    [EnableQuery]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<ProductResponse>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAllProduct()
+    {
+        var resultProduct = await _productServices.GetAllAsync();
+        if (!resultProduct.Any()) {
+            return NotFound();
+        }
+        var resultResponse = _mapper.Map<IEnumerable<ProductResponse>>(resultProduct);
+        return Ok(resultResponse);
+    }
+    [HttpGet("{id}")]
+    [EnableQuery]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<ProductResponse>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAllProductByCategoryId(int id)
+    {
+        var products = await _productServices.GetAllAsync();
+        var filteredProducts = products.Where(p => p.CategoryId == (uint)id);
+        if (!filteredProducts.Any())
+        {
+            return NotFound();
+        }
+        var resultResponse = _mapper.Map<IEnumerable<ProductResponse>>(filteredProducts);
+        return Ok(resultResponse);
+    }
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateProduct([FromBody]ProductRequest productRequest)
+    {
+        if (productRequest == null||!ModelState.IsValid) {
+            return BadRequest();
+        }
+        var productAdd=_mapper.Map<Product>(productRequest);
+        var result=await _productServices.AddAsync(productAdd);
+        return result>0?Ok():BadRequest();
     }
 }
