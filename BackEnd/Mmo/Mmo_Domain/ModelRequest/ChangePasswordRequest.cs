5db946a76b2d1cbe41a7371ddb686cf4719137f9
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2025-10-19 22:40:51 +0700

    header done

---
+++ BackEnd/Mmo/Mmo_Domain/ModelRequest/ChangePasswordRequest.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mmo_Domain.ModelRequest;
+
+public class ChangePasswordRequest
+{
+    [Required(ErrorMessage = "Account ID is required")]
+    [JsonPropertyName("accountId")]
+    public int AccountId { get; set; }
+
+    [Required(ErrorMessage = "New password is required")]
+    [JsonPropertyName("password")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "New password must be between 6 and 100 characters")]
+    public string NewPassword { get; set; } = null!;
+}
---
+++ BackEnd/Mmo/Mmo_Domain/ModelRequest/RefreshTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmo_Domain.ModelRequest;
+
+public class RefreshTokenRequest
+{
+    [Required(ErrorMessage = "Refresh token is required")]
+    public string RefreshToken { get; set; } = null!;
+}
